dc62726d11814161f18edfafbcfc745665e7f577
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-10 15:54:39 +0530

    initializing project

---
+++ MLHerokuProject/sentiment_analysis/herokuenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shree/Documents/javascript-project/PythonProjects/myPythonProgs/MLHerokuProject/sentiment_analysis/herokuenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
